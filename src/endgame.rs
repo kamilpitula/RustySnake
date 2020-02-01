23ca93b00b2b815c2b7f090ec17ba4c2712ce8ca
Kamil Pituła <pitula.k@gmail.com>
2020-02-01 19:11:44 +0100

    scores rendering method divided

--- src/endgame.rs
+++ src/endgame.rs
@@ -32,6 +32,26 @@ impl EndGame{
             top_ten: (*score_controller.borrow_mut()).get_top_scores(10)
         }
     }
+
+    fn render_your_score(&mut self, args: &RenderArgs, glyphs: &mut GlyphCache){
+
+        let u_score = self.data.score.to_string();
+        let your_score =  &("Your score: ".to_owned() + &u_score);
+        self.writer.render_text(args, &mut self.gl, glyphs, colors::RED, 42, 250.0, 70.0, "GAME OVER");
+
+        self.writer.render_text(args, &mut self.gl, glyphs, colors::RED, 32, 250.0, 120.0, your_score);
+    }
+
+    fn render_top_scores(&mut self, args: &RenderArgs, glyphs: &mut GlyphCache){
+
+        let scores = &self.top_ten;
+        for (index, score) in scores.iter().enumerate() {
+            let score = &((index + 1).to_string() + ". " + &score);
+            let pos_y = (260 + (30 * index));
+
+            self.writer.render_text(args, &mut self.gl, glyphs, colors::BLACK, 24, 250.0, pos_y as f64, score);
+        }
+    }
 }
 
 impl GameState for EndGame{
@@ -42,19 +62,8 @@ impl GameState for EndGame{
                 clear(colors::GRAY, gl);
             });
             
-            let u_score = self.data.score.to_string();
-            let your_score =  &("Your score: ".to_owned() + &u_score);
-            self.writer.render_text(args, &mut self.gl, glyphs, colors::RED, 42, 250.0, 70.0, "GAME OVER");
-            
-            self.writer.render_text(args, &mut self.gl, glyphs, colors::RED, 32, 250.0, 120.0, your_score);
-
-            let scores = &self.top_ten;
-            for (index, score) in scores.iter().enumerate() {
-                let score = &((index + 1).to_string() + ". " + &score);
-                let pos_y = (260 + (30 * index));
-
-                self.writer.render_text(args, &mut self.gl, glyphs, colors::BLACK, 24, 250.0, pos_y as f64, score);
-            }
+            self.render_your_score(args, glyphs);
+            self.render_top_scores(args, glyphs);
     }
 
     fn update(&mut self, args: &UpdateArgs) -> State<GameData>{
